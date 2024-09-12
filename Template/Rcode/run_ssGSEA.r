2ffc6145bfef416596e028e331d0fc0d4e67ce00
Yann Vanrobaeys <yann.vanrobaeys@gmail.com>
2024-09-12 09:38:30 -0400

    rm

--- Template/Rcode/run_ssGSEA.r
+++
@@ -1,8 +0,0 @@
-
-ssGSEA.project.dataset(input.ds = "XXX.gct", output.ds = "customSets_ssGSEA", gene.sets.dbfile.list = "customSets.gmx")
-
-rc.toPlot <- calculate_zscore(toPlot)
-round(mean(rowMeans(rc.toPlot)),2)
-round(mean(rowVars(rc.toPlot)),2)
-
-writeLines(capture.output(sessionInfo()), "sessionInfo.txt")
